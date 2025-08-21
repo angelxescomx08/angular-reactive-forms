49e7ffc3e1245e5e8ae768819a0be1fea0ae7693
José Ángel Hernández Radilla <jose.angel.hdz.rda@gmail.com>
2025-08-21 11:01:54 -0600

    Refactor dynamic-page component to utilize reactive forms. Updated HTML to bind form controls and display validation messages dynamically. Enhanced TypeScript logic for form initialization and validation handling.

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,12 +1,25 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.component.html',
   styleUrl: './dynamic-page.component.css'
 })
 export class DynamicPageComponent {
+  private fb = inject(FormBuilder);
 
+  myForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)], []],
+    favoritesGames: this.fb.array([
+      ['Metal Gear', [Validators.required]],
+      ['Final Fantasy VII', [Validators.required]],
+    ], [Validators.minLength(2)]),
+  });
+
+  get favoriteGames() {
+    return this.myForm.get('favoritesGames') as FormArray;
+  }
 }
