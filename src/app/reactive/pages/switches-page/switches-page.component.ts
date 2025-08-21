69a9b3b99c07680c87139773425b6197ec903804
José Ángel Hernández Radilla <jose.angel.hdz.rda@gmail.com>
2025-08-21 14:49:01 -0600

    Refactor switches-page component to utilize reactive forms. Updated HTML to bind form controls for gender, notifications, and terms acceptance. Enhanced TypeScript logic for form initialization and submission handling.

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,12 +1,25 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.component.html',
   styleUrl: './switches-page.component.css'
 })
 export class SwitchesPageComponent {
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
 
+  myForm = this.fb.group({
+    gender: ['M', [Validators.required]],
+    notifications: [true, [Validators.required]],
+    termsAndConditions: [false, [Validators.requiredTrue]],
+  });
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }
