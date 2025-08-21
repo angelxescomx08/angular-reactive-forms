44f6e3562358e06afd8af44ac3cc5b6efcba08ee
José Ángel Hernández Radilla <jose.angel.hdz.rda@gmail.com>
2025-08-21 11:35:48 -0600

    Enhance dynamic-page component to display dynamic validation messages for form fields. Updated HTML to utilize FormUtils for error handling and improved TypeScript logic for form validation in both individual and array fields.

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,22 +1,32 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormArray, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-dynamic-page',
   imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.component.html',
-  styleUrl: './dynamic-page.component.css'
+  styleUrl: './dynamic-page.component.css',
 })
 export class DynamicPageComponent {
   private fb = inject(FormBuilder);
+  formUtils = FormUtils;
 
   myForm = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)], []],
-    favoritesGames: this.fb.array([
-      ['Metal Gear', [Validators.required]],
-      ['Final Fantasy VII', [Validators.required]],
-    ], [Validators.minLength(2)]),
+    favoritesGames: this.fb.array(
+      [
+        ['Metal Gear', [Validators.required]],
+        ['Final Fantasy VII', [Validators.required]],
+      ],
+      [Validators.minLength(2)]
+    ),
   });
 
   get favoriteGames() {
--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,17 +1,7 @@
-import { FormGroup } from "@angular/forms";
+import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 
 export class FormUtils {
-  static isValidField(form: FormGroup, field: string): boolean | null {
-    return (
-      !!form.controls[field].errors && form.controls[field].touched
-    );
-  }
-
-  static getFieldError(form: FormGroup, field: string): string | null {
-    if (!form.controls[field].errors) return null;
-
-    const errors = form.controls[field].errors || {};
-
+  static getTextError(errors: ValidationErrors) {
     for (const key of Object.keys(errors)) {
       switch (key) {
         case 'required':
@@ -25,5 +15,29 @@ export class FormUtils {
       }
     }
     return null;
+  }
+
+  static isValidField(form: FormGroup, field: string): boolean | null {
+    return !!form.controls[field].errors && form.controls[field].touched;
+  }
+
+  static getFieldError(form: FormGroup, field: string): string | null {
+    if (!form.controls[field].errors) return null;
+
+    const errors = form.controls[field].errors || {};
+
+    return this.getTextError(errors);
+  }
+
+  static isValidFieldInArray(form: FormArray, index: number) {
+    return form.controls[index].errors && form.controls[index].touched;
+  }
+
+  static getArrayFieldError(form: FormArray, index: number): string | null {
+    if (form.controls.length === 0) return null;
+
+    const errors = form.controls[index].errors || {};
+
+    return this.getTextError(errors);
   }
 }
